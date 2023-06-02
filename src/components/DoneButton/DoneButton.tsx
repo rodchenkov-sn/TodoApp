8476841932937fe4b3263b043fa47ad9323e2848
rodchenkov-sn <serod2015@hotmail.com>
2023-06-03 01:50:28 +0300

    actually delete done button component

--- src/components/DoneButton/DoneButton.tsx
+++
@@ -1,8 +0,0 @@
-interface DoneButtonProps {
-  done: boolean
-  onClicked: () => void
-}
-
-export default function DoneButton({ done, onClicked }: DoneButtonProps) {
-  return <button onClick={onClicked}>{done ? "Undone" : "Done"}</button>
-}
