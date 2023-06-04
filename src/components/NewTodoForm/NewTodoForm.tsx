db18559d423a2d33104ec87c941e3915cbb3caed
rodchenkov-sn <serod2015@hotmail.com>
2023-06-04 18:30:34 +0300

    add custom css to the new item component

--- src/components/NewTodoForm/NewTodoForm.tsx
+++ src/components/NewTodoForm/NewTodoForm.tsx
@@ -1,5 +1,7 @@
 import { useDispatch, useSelector } from "react-redux"
 
+import style from "./NewTodoForm.module.scss"
+
 import { updateContent } from "@/state/NewItemContentState/Slice"
 import { RootState } from "@/state/Store"
 import { addItem } from "@/state/TodoItemsState/Slice"
@@ -10,17 +12,17 @@ export default function NewTodoForm() {
   const dispatch = useDispatch()
 
   return (
-    <>
+    <form
+      className={style.newTodoForm}
+      onSubmit={e => {
+        e.preventDefault()
+        if (content !== "") {
+          dispatch(addItem({ content }))
+          dispatch(updateContent(""))
+        }
+      }}>
       <input color="dark" value={content} onChange={e => dispatch(updateContent(e.target.value))} />
-      <button
-        onClick={() => {
-          if (content !== "") {
-            dispatch(addItem({ content }))
-            dispatch(updateContent(""))
-          }
-        }}>
-        Add
-      </button>
-    </>
+      <button type="submit">Add</button>
+    </form>
   )
 }
