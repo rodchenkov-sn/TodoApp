0c5ad843dfd3e7ff193e69505815ab2f451004cc
rodchenkov-sn <serod2015@hotmail.com>
2023-06-04 20:17:10 +0300

    repalce new item content state with useRef hook

--- src/state/Store.ts
+++ src/state/Store.ts
@@ -1,21 +1,18 @@
 import { AnyAction, ThunkDispatch, configureStore } from "@reduxjs/toolkit"
 
-import newItemContentReducer from "./NewItemContentState/Slice"
 import { saveItemsToLocalStore, loadItemsFromLocalStore } from "./TodoItemsState/Middleware"
 import { todoItemsReducer, TodoItemsState } from "./TodoItemsState/Slice"
 
 export const store = configureStore({
   preloadedState: {
     todoItems: { items: loadItemsFromLocalStore() },
-    newItemContent: "",
   },
   reducer: {
     todoItems: todoItemsReducer,
-    newItemContent: newItemContentReducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(saveItemsToLocalStore.middleware),
 })
 
-export type RootState = { todoItems: TodoItemsState; newItemContent: string }
+export type RootState = { todoItems: TodoItemsState }
 
 export type AppDispatch = ThunkDispatch<RootState, undefined, AnyAction>
